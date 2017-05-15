42d61f7fdf88864e645a343d45f2641c71aa8450
job <job@vv32.nl>
2017-05-15 12:17:40 +0200

    code gefixt

--- src/edu/mass/necc/processing/Sketch.java
+++ src/edu/mass/necc/processing/Sketch.java
@@ -1,85 +1,58 @@
 package edu.mass.necc.processing;
 
 import processing.core.PApplet;
+import processing.serial.Serial;
 
-public class Sketch extends PApplet {
+final public class Sketch extends PApplet {
 
     @Override
     public void settings() {
         super.size(1280, 720);
     }
     
+    final private Serial myPort = new Serial(this, "/dev/ttyACM1", 2400);
+    {
+        myPort.bufferUntil('.');
+    }
+
     @Override
     public void setup() {
         super.surface.setResizable(true);
-        super.frame.setTitle("H&K Zonnekrachtmeter");
-        
+        super.surface.setTitle("H&K Zonnekrachtmeter");
+        super.smooth();
+        Images.setup(this);
     }
+    
+    String data="";
+    String potmeter="";
+    float wPotmeter = 50;
+
+    float[] arrayOfFloats = new float[200];
     
     @Override
     public void draw() {
-        super.rect(100, 100, 100, 100);
+        super.background(200);
+        super.image(Images.LOGO, width-142, 0, 132, 100);
+        tekenGrafiek();
+    }
+    
+    private void tekenGrafiek() {
+        for(int i=0; i<arrayOfFloats.length-1; i++)
+            arrayOfFloats[i] = arrayOfFloats[i+1];
+
+        arrayOfFloats[arrayOfFloats.length-1] = wPotmeter;
+
+        for(int i=0; i<arrayOfFloats.length; i++) {
+            noStroke();
+            fill(0,255,0,255);
+            ellipse(width*i/arrayOfFloats.length+width/200/2,height-arrayOfFloats[i],width/200,width/200);
+        }
+    }
+    
+    public void serialEvent(Serial myPort) {
+        data = myPort.readStringUntil('.');
+        data = data.substring(0, data.length()-1);
+        wPotmeter = Float.parseFloat(data);
+        wPotmeter = map(wPotmeter, 0, 4096, 0, height);
     }
 }
-
-import processing.serial.*;
-import java.awt.event.KeyEvent; 
-Serial myPort;
-
-String data="";
-String potmeter="";
-float wPotmeter = 50;
-
-int num = 200;
-float[] arrayOfFloats = new float[num];
-
-PImage logo;
-
-//int i;
-void setup() {
-  size(1200,800);
-  surface.setResizable(true);
-  smooth();
-  background(0);
-   myPort = new Serial(this,"COM4", 2400); 
-   myPort.bufferUntil('.');
-  logo = loadImage("henklogo.png");
-
-}
-
- void draw() {
-   background(200);
- image(logo, width-142, 00,132,100);
- tekenGrafiek();
-
- }
- 
-void serialEvent (Serial myPort){
-  data = myPort.readStringUntil('.');
-  data = data.substring(0,data.length()-1);
-  wPotmeter = float(data);
-  wPotmeter = map(wPotmeter,0, 4096,0, height);
-
- }  
-
-void tekenGrafiek(){
-
-  for (int i=0; i<arrayOfFloats.length-1; i++) {
-    arrayOfFloats[i] = arrayOfFloats[i+1];
-  }
- 
-
-  float newValue = wPotmeter;
- 
- 
-  arrayOfFloats[arrayOfFloats.length-1] = newValue;
- 
-  
-  for (int i=0; i<arrayOfFloats.length; i++) {
-    noStroke();
-    fill(0,255,0,255);
-    ellipse(width*i/arrayOfFloats.length+width/num/2,height-arrayOfFloats[i],width/num,width/num);
-  }
-}
-  
-  
